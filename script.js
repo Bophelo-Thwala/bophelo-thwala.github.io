ad93e23fe21c169f6f36f44bfdeb430448ee6607
Bophelo-Thwala <bophelokal@gmail.com>
2025-06-13 17:57:56 +0200

    Updated assets

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ function RedirectToHomePage() {
     window.location.href ="pages/home.html";
 }
 
+function OpenHomePageInNewTab() {
+    window.open("pages/home.html", "_blank");
+    RedirectToHomePage();
+}
+
 console.log("\nWidth: " + window.innerWidth + "px");
 console.log("\nHeight: " + window.innerHeight + "px");
 
 RedirectToHomePage();
+
+
 
 
